76eca4a7595797e1234d78c4e0ea3b4fef29fab5
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-19 16:49:14 -0300

    atualizado

--- src/Game.tsx
+++ src/Game.tsx
@@ -9,16 +9,20 @@ interface GameProp{
 
 export function Game() {
   const [game, setGame] = useState<GameProp | null>(null)
-
+  const [errorMsg, setErrorMsg] = useState("")
 
   useEffect(() => {
 
     async function fetchGame(){
-      const response = await api.get("/next-api/?api=game&id=15")
-      setGame({
-        title: response.data.title,
-        image_url: response.data.image_url
-      })
+      try{
+        const response = await api.get("/next-api/?api=game&id=15")
+        setGame({
+          title: response.data.title,
+          image_url: response.data.image_url
+        })
+      }catch(err){
+        setErrorMsg("Erro ao buscar dados")
+      }
     }
 
     fetchGame();
@@ -31,12 +35,15 @@ export function Game() {
       {game && (
         <>
           <Image
+            testID="avatarGame"
             source={{ uri: game.image_url }}
             style={{ width: 70, height: 70, borderRadius: 99 }}
           />
           <Text>{game.title}</Text>
         </>
       )}
+
+      {errorMsg !== "" && <Text>{errorMsg}</Text>}
 
    </View>
   );
