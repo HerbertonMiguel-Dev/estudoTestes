d97ca5a5652164f400b44a60fd688e372c95793f
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-19 17:10:30 -0300

    feito os test de falha res http

--- __test__/game/game.test.tsx
+++ __test__/game/game.test.tsx
@@ -5,12 +5,12 @@ import { Game } from '../../src/Game'
 
 const mock = new AxiosMockAdapter(api)
 
-describe("Game component tests", () => {
+describe("Testes de componentes do Game", () => {
   afterEach(() => {
     mock.reset();
   })
 
-  it("renders game data correctly on Api call", async () => {
+  it("renderiza os dados do jogo corretamente na chamada da API", async () => {
     mock.onGet("/next-api/?api=game&id=15").reply(200, {
       title: "Jogo Mock Teste",
       image_url: "https://sujeitoprogramador.com/next-api/foto15.png"
@@ -22,6 +22,18 @@ describe("Game component tests", () => {
       expect(getByText("Jogo Mock Teste")).toBeTruthy();
       expect(getByTestId("avatarGame").props.source.uri).toBe("https://sujeitoprogramador.com/next-api/foto15.png")
     }) 
+
+  })
+
+  it("deve exibir uma mensagem de erro quando a chamada da API falhar", async () => {
+    mock.onGet("/next-api/?api=game&id=15").networkError();
+
+    const { findByText } = render(<Game/>)
+
+    //Esperar pela mensagem de erro na tela.
+    const erroMessage = await findByText("Erro ao buscar dados")
+    expect(erroMessage).toBeTruthy();
+
 
   })
 
