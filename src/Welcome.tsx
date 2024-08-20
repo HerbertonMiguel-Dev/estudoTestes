83eaaec72e988d5a238d9563576294dda808db3d
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-08-20 10:09:28 -0300

    criado component e importado no App.tsx

---
+++ src/Welcome.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+export function Welcome({ handleGetUser }: { handleGetUser: () => void }) {
+ const [input, setInput] = useState("")
+ const [user, setUser] = useState("")
+
+  function handleShowUser(){
+    if(input === ""){
+      setUser("");
+      return;
+    }
+
+    let message = "Bem vindo " + input
+    setUser(message);
+    handleGetUser();
+
+  }
+
+ return (
+   <View style={styles.container}>
+    <TextInput
+      placeholder="Digite seu nome..."
+      value={input}
+      onChangeText={ (text) => setInput(text) }
+      style={styles.input}
+    />
+
+    <Button title="Login" onPress={handleShowUser}/>
+
+    {user !== "" && <Text>{user}</Text>}
+
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    width: '90%',
+    marginTop: 24,
+    gap: 8
+  },
+  input:{ 
+    borderWidth: 1,
+    padding: 4,
+  }
+})
